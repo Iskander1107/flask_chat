68ee7949d9179a5d9371536a7312bf19bfea7ee9
alimov iskander <alimov.ikh@phystech.edu>
2022-05-01 22:40:42 +0300

    add

--- server/app/models.py
+++ server/app/models.py
@@ -1,11 +1,11 @@
-from app import db
+# models.py
 
+from flask_login import UserMixin
+from . import db
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
 
-def __repr__(self):
-        return '<User {}>'.format(self.username)
